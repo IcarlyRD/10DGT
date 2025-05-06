7c7014a7203cb63520cc2e8a83a49a68c3f4a436
IcarlyRD <elaine.labuschagne@kingsway.school.nz>
2025-05-07 10:55:29 +1200

    Add files via upload
    How to use inputs from a user and store it.

---
+++ inputs and loops.py
@@ -0,0 +1,23 @@
+# To learn how to use inputs and loops
+# Author: Elaine labuschagne 
+# Date: 07/05/2025
+# Version 1
+
+#Ask the user a question and store their answer in a variable
+# Ask the user for their name and Store 
+name = input ("Hi, what is your name? ") # Stores answer as a string
+
+# Ask the user for two numbers and then add them togther.
+num1 = int (input(" what is your first number please? "))
+num2 = int (input("what is your second number please? "))
+print(f" You entered your first number as {num1}")
+print(f"You entered your second number as { num2}")
+
+# Adding the answers togther 
+sum =num1+ num2
+print(sum)
+
+# Test that input was stored correctly
+print(name)
+# to comment code out, use crtl+/
+
